5debf9d0577fdb73eeba675a78051ebb02b9803a
fggggggggggggggggg <ffffffffffff>
2016-01-11 16:27:41 +0100

    code fb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -202,6 +202,33 @@
         </div><!-- /.col-lg-3 -->
       
            
+           
+<!-- FACEBOOK CODE API-->
+<!--  <div id="fb-root"></div>
+<script>
+    /* Facebook Like Button*/
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    /*Google+ */
+    (function () {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+</script>-->
+
+<!-- End of FACEBOOK CODE -->
+           
           
           
       </div><!-- /.row -->
